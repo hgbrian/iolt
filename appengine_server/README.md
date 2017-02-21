44f360f48fe7108522f610b5944bf45df59c5a82
Brian Naughton <briannaughton@gmail.com>
2017-02-20 16:16:13 -0800

    plot data on appengine

---
+++ appengine_server/README.md
@@ -0,0 +1,3 @@
+http://mcaule.github.io/d3-timeseries/
+dev_appserver.py app.yaml
+gcloud app deploy app.yaml
--- appengine_server/main.py
+++ appengine_server/main.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 import webapp2
 import json
+import os
+from datetime import timedelta
+
+from google.appengine.ext.webapp import template, util
 from google.appengine.ext import ndb
 
 
@@ -14,10 +18,34 @@ class MainPage(webapp2.RequestHandler):
         data = json.loads(self.request.body)
         dat = Reading(data=json.dumps(data))
         dat.put()
-        #else:
-        #    print("no temperature")
-        #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.write('Hello, World!')
+
+    def get(self):
+        """reading every 10 minutes of temperature (C) and humidity (RH)"""
+        N = 10 # from which, take the median, percentiles
+        datastr = []
+        for d in Reading.query():
+            cdate = d.date - timedelta(hours=8) # timezones are a major pain in appengine
+            th = json.loads(d.data)
+            ts = [_t*1.8+32 for _t in th[-2::-2]] # C to F
+            hs = th[-1::-2]
+            for n in range(0, len(ts), N):
+                cdate = cdate - timedelta(minutes=10)
+                tn = ts[n:n+N]
+                tn.sort()
+                t_med = tn[len(tn)//2]
+                t1, t9 = tn[0], tn[-1]
+                
+                hn = hs[n:n+N]
+                hn.sort()
+                h_med = hn[len(hn)//2]
+                h1, h9 = hn[0], hn[-1]
+                
+                datastr.append("{date:new Date('%s'),t:%s,h:%s,t_ci_down:%s,t_ci_up:%s,h_ci_down:%s,h_ci_up:%s}," %
+                    (cdate, t_med, h_med, t1, t9, h1, h9))
+        
+        path = os.path.join(os.path.dirname(__file__), "index.html")
+        index = template.render(path, {'data': '[' + '\n'.join(sorted(datastr)) + ']'})
+        self.response.out.write(index)
 
 
 app = webapp2.WSGIApplication([
