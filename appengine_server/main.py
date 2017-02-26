6caa6d9a3baff4bd1c332b7cc95fa86afdeb4d88
Brian Naughton <briannaughton@gmail.com>
2017-02-26 09:53:04 -0800

    appengine allows for thermometer name

--- appengine_server/main.py
+++ appengine_server/main.py
@@ -10,20 +10,22 @@ from google.appengine.ext import ndb
 
 class Reading(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True, indexed=False)
+    name = ndb.TextProperty(indexed=False, default='')
     data = ndb.TextProperty(indexed=False, default='')
 
 
 class MainPage(webapp2.RequestHandler):
-    def post(self):
+    def post(self, tname):
         data = json.loads(self.request.body)
-        dat = Reading(data=json.dumps(data))
+        dat = Reading(name=tname, data=json.dumps(data))
         dat.put()
 
-    def get(self):
+    def get(self, tname):
         """reading every 10 minutes of temperature (C) and humidity (RH)"""
-        N = 10 # from which, take the median, percentiles
+        N = 10 # take the median, percentiles
         datastr = []
         for d in Reading.query():
+            cname = d.name
             cdate = d.date - timedelta(hours=8) # timezones are a major pain in appengine
             th = json.loads(d.data)
             ts = [_t*1.8+32 for _t in th[-2::-2]] # C to F
@@ -40,8 +42,8 @@ class MainPage(webapp2.RequestHandler):
                 h_med = hn[len(hn)//2]
                 h1, h9 = hn[0], hn[-1]
                 
-                datastr.append("{date:new Date('%s'),t:%s,h:%s,t_ci_down:%s,t_ci_up:%s,h_ci_down:%s,h_ci_up:%s}," %
-                    (cdate, t_med, h_med, t1, t9, h1, h9))
+                datastr.append("{n:'%s',date:new Date('%s'),t:%s,h:%s,t_ci_down:%s,t_ci_up:%s,h_ci_down:%s,h_ci_up:%s}," %
+                    (cname, cdate, t_med, h_med, t1, t9, h1, h9))
         
         path = os.path.join(os.path.dirname(__file__), "index.html")
         index = template.render(path, {'data': '[' + '\n'.join(sorted(datastr)).rstrip(',') + ']'})
@@ -49,5 +51,5 @@ class MainPage(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/([^/]*)', MainPage),
 ], debug=True)
